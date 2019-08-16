b787768a14a4dc539c7a8cca80d2d833d583bf08
zzy <zhaozhengyan@live.com>
2019-08-16 17:33:24 +0800

    update starup

--- Nexter.FinTech/Nexter.FinTech/Startup.cs
+++ Nexter.FinTech/Nexter.FinTech/Startup.cs
@@ -84,11 +84,11 @@ namespace Nexter.FinTech
             }
             else
             {
+                app.UseHttpsRedirection();
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
             app.UseMvc();
         }
     }
