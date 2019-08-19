3959ef3133cf246b6415ce5c5b2ddbe04d4615e8
zzy <zhaozhengyan@live.com>
2019-08-19 17:02:45 +0800

    clear code

--- Nexter.FinTech/Nexter.FinTech/Controllers/TransactionController.cs
+++ Nexter.FinTech/Nexter.FinTech/Controllers/TransactionController.cs
@@ -25,7 +25,6 @@ namespace Nexter.FinTech.Controllers
         [HttpGet]
         public async Task<Result> GetAsync([FromQuery] TransactionQuery query)
         {
-            throw new Exception("test");
             var session = this.GetSession();
             var startDate = DateTime.Now.FirstDayOfThisMonth();
             var endDate = DateTime.Now.LastDayOfThisMonth();
